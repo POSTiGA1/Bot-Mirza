423696f5525b9c21565ee13597a0dc1f90e63fbe
Mahdi <mahdigholipour@outlook.com>
2026-06-25 23:29:20 +0330

    refactor: improve readability and structure of uptime_node.php

--- cronbot/uptime_node.php
+++ cronbot/uptime_node.php
@@ -15,14 +15,20 @@ $status_cron = json_decode($setting['cron_status'],true);
 if(!$status_cron['uptime_node'])return;
 $marzbanlist = select("marzban_panel", "*","type" ,"marzban" ,"fetchAll");
 $inbounds = [];
-foreach($marzbanlist as $location){
-$Getdnodes = Get_Nodes($location['name_panel']);
-if(!empty($nodes['error']))continue;
-if(!empty($nodes['status'])  && $nodes['status'] != 200 )continue;
-$Getdnodes = json_decode($Getdnodes['body'],true);
-if(count($Getdnodes) == 0)return;
-foreach($Getdnodes as $data){
-    if(!in_array($data['status'],["connected","disabled"])){
+foreach ($marzbanlist as $location) {
+    $Getdnodes = Get_Nodes($location['name_panel']);
+    if (!empty($Getdnodes['error']))
+        continue;
+    if (!empty($Getdnodes['status']) && $Getdnodes['status'] != 200)
+        continue;
+    $Getdnodes = json_decode($Getdnodes['body'], true);
+    if (count($Getdnodes) == 0)
+        return;
+    if ($location['version_panel'] == "1") {
+        $Getdnodes = $Getdnodes['nodes'];
+    }
+    foreach ($Getdnodes as $data) {
+        if (!in_array($data['status'], ["connected", "disabled"])) {
             $textnode = sprintf($textbotlang['hardcoded']['nodeDownNotice'], $data['name'], $data['status'], $data['message']);
         if (strlen($setting['Channel_Report']) > 0) {
         telegram('sendmessage',[
